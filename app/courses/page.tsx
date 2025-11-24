4769ed54a31f5449c79dc99db103a0003a5f81a2
Adill453 <el.khanchoufi.org@gmail.com>
2025-11-24 15:48:11 +0100

    changer le style de cours!!

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -43,7 +43,7 @@ export default function CoursesPage() {
     }
   }
 
-  // Booking is now handled by the BookingModal component
+  // Booking is now handled by theee BookingModal component
 
   return (
     <div className="min-h-screen bg-background">
